20abe982ce852877c1f4a26329e688556bf7adb1
Stefan Geyer <git@stefan-geyer.org>
2020-03-21 17:23:55 +0100

    remove not wirking include

--- project/dict_generator.py
+++ project/dict_generator.py
@@ -1,5 +1,5 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QStringList
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
 from PyQt5.QtGui import QIcon
 
 Form, Window = uic.loadUiType("test_gui.ui")
