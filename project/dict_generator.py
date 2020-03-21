e6aa2ac0c25ea5c1769544b41d8c839e820cd927
Stefan Geyer <git@stefan-geyer.org>
2020-03-21 17:34:47 +0100

    add load dialog

--- project/dict_generator.py
+++ project/dict_generator.py
@@ -1,6 +1,8 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
 from PyQt5.QtGui import QIcon
+import json
+
 
 Form, Window = uic.loadUiType("test_gui.ui")
 
@@ -10,7 +12,20 @@ def safeDialog(self):
     pass
 
 def openDialog(self):
-    pass
+    dlg = QFileDialog()
+    dlg.setFileMode(QFileDialog.AnyFile)
+    dlg.setNameFilter("Json files (*.json)")
+    # filenames = QStringList()
+
+    if dlg.exec_():
+      filenames = dlg.selectedFiles()
+      f = open(filenames[0], 'r')
+
+      with f:
+         data = f.read()
+         d = json.loads(data)
+         global dict_state
+         dict_state = d
 
 def newDictDialog(self):
     pass
