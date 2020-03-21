77eeac0b7bd7035171b45df28622a1dd1fd962f5
Stefan Geyer <git@stefan-geyer.org>
2020-03-21 21:47:40 +0100

    add table function

--- project/dict_generator.py
+++ project/dict_generator.py
@@ -1,6 +1,6 @@
 import json
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QTableWidgetItem
 from PyQt5.QtGui import QIcon
 
 Form, Window = uic.loadUiType("test_gui.ui")
@@ -31,13 +31,35 @@ def openDialog():
          data = f.read()
          d = json.loads(data)
          global dict_state
-         dict_state = d
+         if 'language_map' in d:
+            dict_state = d
+
+    updatedTable()
 
 def newDictDialog():
     pass
 
 def updatedTable():
-    pass
+
+    global dict_state
+    form.dict_table.setRowCount(300)
+    form.dict_table.setColumnCount(len(dict_state['language_map']))
+
+
+    row_count = 0
+    for element in dict_state['question_map']:
+        id = element['question_id']
+        col_count = 0
+        for key, value in dict_state['language_map'].items():
+            if id in value:
+                form.dict_table.setItem(row_count, col_count, QTableWidgetItem(value[id]))
+            col_count += 1
+        row_count += 1
+    form.dict_table.resizeColumnsToContents()
+    form.dict_table.resizeRowsToContents()
+
+def handleTableClick(row, column):
+    writeInputToDict(row)
 
 def writeInputToDict(idx):
     pass
@@ -50,7 +72,7 @@ form.setupUi(window)
 form.load_dict.clicked.connect(openDialog)
 form.save_dict.clicked.connect(safeDialog)
 form.new_dict.clicked.connect(newDictDialog)
-
+form.dict_table.cellClicked.connect(handleTableClick)
 
 window.show()
 app.exec_()
