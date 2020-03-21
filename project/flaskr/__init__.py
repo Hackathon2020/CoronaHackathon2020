5ec91785206e26d661bfc7349c3362f8a8cc21ef
Maxi B <beiersmaxi@gmail.com>
2020-03-21 15:20:46 +0100

    Super Simple Example Routing

--- project/flaskr/__init__.py
+++ project/flaskr/__init__.py
@@ -2,21 +2,21 @@ import os
 import pathlib
 from flask import Flask, render_template
 
+
 app = Flask(__name__, instance_relative_config=True)
-module_path = os.path.abspath(os.path.dirname(__file__))
 
 @app.route('/')
+@app.route('/index')
 #@app.route('/index')
 def index():
-    return '''
-    <html>
-    <head>
-        <title>Home Page</title>
-    </head>
-    <body>
-        <h1>Hello!</h1>
-    </body>
-    </html>
-    '''
-    #render_template(f"{module_path}/templates/app/index.html", title="Home")
+    return render_template("app/index.html", title="Home")
 
+
+@app.route('/answer')
+def answer():
+    return render_template("app/answer.html", title="Formula1")
+
+
+@app.route('/question')
+def question():
+    return render_template("app/question.html", title="Formula2")
