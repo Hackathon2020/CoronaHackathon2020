1f6595f7d193b1a1daa20e8cb7a22531ff0d5ea5
Stefan Geyer <git@stefan-geyer.org>
2020-03-21 17:22:48 +0100

    add update function for table and connect buttons

--- project/dict_generator.py
+++ project/dict_generator.py
@@ -1,11 +1,33 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QStringList
+from PyQt5.QtGui import QIcon
 
 Form, Window = uic.loadUiType("test_gui.ui")
+
+global dict_state
+
+def safeDialog(self):
+    pass
+
+def openDialog(self):
+    pass
+
+def newDictDialog(self):
+    pass
+
+def updatedTable(self):
+    pass
 
 app = QApplication([])
 window = Window()
 form = Form()
 form.setupUi(window)
+
+form.loadDict.clicked.connect(openDialog)
+form.saveDict.clicked.connect(safeDialog)
+form.newDict.clicked.connect(newDictDialog)
+
 window.show()
 app.exec_()
+
+
