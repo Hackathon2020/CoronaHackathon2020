dfb739d2b5eaffe3b223aa5f5f97cd2a75d0f608
Maxi B <beiersmaxi@gmail.com>
2020-03-21 21:00:02 +0100

    information.html added as index

--- project/flaskr/app.py
+++ project/flaskr/app.py
@@ -1,24 +1,24 @@
 # TODO: Import  muss noch angepasst werden, wenn von Konsole gestartet wird
 import os
 import pathlib
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from project.flaskr.services import forms
 from project.questionaire import Questionaire, Question
 
 
-project_dir = pathlib.Path(os.path.abspath(os.path.dirname(__file__)), "templates")
-
-app = Flask(__name__, template_folder=project_dir)
+project_dir = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
+app = Flask(__name__, template_folder=f"{project_dir}/templates", static_folder=f"{project_dir}/static")
 SECRET_KEY = os.urandom(32)
 app.config["SECRET_KEY"] = SECRET_KEY
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 @app.route('/index')
 #@app.route('/index')
 def index():
     LANGUAGES = ["English", "German", "Spanish", "French"]
     BORDERS = ["England", "Germany", "Spain", "Poland", "France"]
-    return render_template("app/index.html", title="Home", languages=LANGUAGES, borders=BORDERS)
+    return render_template("app/informations.html")
+    #return render_template("app/index.html", title="Home", languages=LANGUAGES, borders=BORDERS)
 
 
 @app.route('/answer')
