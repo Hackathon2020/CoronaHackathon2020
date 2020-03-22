f82a866d0dd87139f07c4b6caebf8111d192bfbd
Maxi B <beiersmaxi@gmail.com>
2020-03-22 10:21:42 +0100

    Refactoring continued

--- project/flaskr/routes/questionaire.py
+++ project/flaskr/routes/questionaire.py
@@ -7,15 +7,16 @@ from project.questions_from_json import read
 
 def get_questionaire_blueprint(language="English"):
     questionnair = Blueprint('questionnair', __name__, template_folder=template_folder, static_folder=static_folder)
-    language_map = {"English": {"1": "test question?"}}
-    questions = [Question("1", "string")]
+    language_map = {"English": {"1": "test question?", "2": "Another Question!"}}
+    questions = [Question("1", "string"), Question("2", "String")]
     questionaire = Questionaire("global_id", language_map, questions)
-    localized_questions = questionaire.localized_questions(language)[0]
-    localized_questions.options_text = ["A", "B", "C"]
+    localized_questions = questionaire.localized_questions(language)
+    localized_questions[0].options_text = ["A", "B", "C"]
+    localized_questions[1].options_text = ["A", "B", "C"]
     #questionaire = read("/home/maximilianbeier/ws/CoronaHackathon2020/json_schemas/questionaire_example.json")
     @questionnair.route("/questionnair")
     def questionnaire_begin():
-            return render_template("questionnair.html", options=localized_questions.options_text)
+            return render_template("questionnair.html", questions=localized_questions[0])
 
 
 
