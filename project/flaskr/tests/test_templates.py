66621097d1c55c04e2e82738646cf2f0f3a3b51b
Maxi B <beiersmaxi@gmail.com>
2020-03-22 13:18:52 +0100

    Template questionnaire for different answer_types

--- project/flaskr/tests/test_templates.py
+++ project/flaskr/tests/test_templates.py
@@ -3,22 +3,19 @@ import pathlib
 import json
 from jinja2 import Template
 from project.questionaire import  Question, Questionaire
+from project.questions_from_json import read
 
 class Jinja2TemplatesTest(unittest.TestCase):
 
     def test_get_result_questionair(self):
+        path_context = pathlib.Path(pathlib.Path(__file__).parent.parent.parent.parent,
+                                    "json_schemas/questionaire_schema.json")
         path_to_json_example_file = pathlib.Path(pathlib.Path(__file__).parent.parent.parent.parent,
                                                  "json_schemas/questionaire_example.json")
-        with open(path_to_json_example_file, 'r') as j:
-            contents = json.loads(j.read())
-            language_map = contents["language_map"]
-            options = ["2", "3", "4"]
-            options1 = ["10", "11", "12"]
-            questions = [Question("1", "string", options), Question("9", "string", options1)]
-            questionaire = Questionaire("global_id", language_map, questions)
-            localized_questions = questionaire.localized_questions("german")
-            path_template = pathlib.Path(pathlib.Path(__file__).parent.parent, "templates/questionnair/questionnair_all_in_one.html")
-            s = Template(str(path_template))
-            abc = s.render(localized_questions)
-            print("DEBUG")
+        questionaire = read(path_to_json_example_file, path_context)
+        localized_questions = questionaire.localized_questions("german")
+        path_template = pathlib.Path(pathlib.Path(__file__).parent.parent, "templates/questionnair/questionnair_all_in_one.html")
+        s = Template(str(path_template))
+        abc = s.render(localized_questions)
+        print("DEBUG")
 
